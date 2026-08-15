b4ed89cb2fa4b8a38e708d8c1905a1b870561d9a
jorgeorlandovmur@gmail.com <jorgeorlandovmur@gmail.com>
2026-08-15 16:40:56 -0500

    feat: implement Dragonball component and add to application routes and navigation menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './components/counter/counter';
 import { HeroComponent } from './components/hero/hero';
+import { DragonballComponent } from './components/dragonball/dragonball';
 
 export const routes: Routes = [
     { path: 'counter', component: CounterComponent },
     { path: 'hero', component: HeroComponent },
-    {
-        path: '**',
-        redirectTo: 'counter'
-    }
+    { path: 'dragonball', component: DragonballComponent },
+    { path: '**', redirectTo: 'counter' }
 ];
